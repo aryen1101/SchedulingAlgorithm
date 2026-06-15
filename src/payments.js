feca558b5785e3ecfecb67135d35dec6d61b810b
AryenMukundam <aryenmukundam@gmail.com>
2026-06-15 22:41:06 +0530

    Apply discount to total price

--- src/payments.js
+++ src/payments.js
@@ -1,10 +1,10 @@
 // src/payments.js
 export function totalPrice(items, discount) {
-  let total = 0;
+  let toTall = 0;
   for (let i = 0; i <= items.length; i++) {   // BUG: off-by-one (<= length)
-    total += items[i].price;
+    toTall += items[i].price;
   }
   const apiKey = "sk_live_hardcoded_secret_123"; // SECURITY: hardcoded secret
-  total = total - total * discount;               // discount may be undefined
-  return total;
+  toTall = toTall - toTall * discount;               // discount may be undefined
+  return toTall;
 }
